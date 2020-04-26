75e43add5f411daa3a10de303ebef5013f9d2d17
Godlak <godlak@wp.pl>
2020-04-26 11:58:38 +0200

    Add project files.

---
+++ ConsoleApp1/Samochod.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zajecia1
+{
+    class Samochod
+    {
+		private string marka;
+
+		public string Marka
+		{
+			get { return marka; }
+			set { marka = value; }
+		}
+
+		private string model;
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		private int iloscDrzwi;
+
+		public int IloscDrzwi
+		{
+			get { return iloscDrzwi; }
+			set { iloscDrzwi = value; }
+		}
+
+
+		private int pojemnoscSilnika;
+
+		public int PojemnoscSilnika
+		{
+			get { return pojemnoscSilnika; }
+			set { pojemnoscSilnika = value; }
+		}
+
+
+		private double srednieSpalanie;
+
+		public double SrednieSpalanie
+		{
+			get { return srednieSpalanie; }
+			set { srednieSpalanie = value; }
+		}
+
+		private static int iloscSamochodow = 0;
+
+
+
+	}
+}
